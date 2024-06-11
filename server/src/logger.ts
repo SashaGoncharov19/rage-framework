0a81acf49e807ef7669ffdfd9b704c80871efa75
Oleksandr Honcharov <0976053529@ukr.net>
2024-06-11 20:28:57 +0300

    logger functionality

---
+++ server/src/logger.ts
@@ -0,0 +1,36 @@
+import winston, { format } from 'winston'
+const { timestamp, printf, colorize } = format
+
+export default class Logger {
+    private format = printf(({ message, level, timestamp }) => {
+        return `[${new Date(timestamp).toLocaleTimeString()}] [${level}]: ${message}`
+    })
+
+    private systemLogger = winston.createLogger({
+        transports: [new winston.transports.Console()],
+        format: format.combine(
+            colorize({
+                level: true,
+                colors: {
+                    error: 'red',
+                    warn: 'yellow',
+                    info: 'white',
+                },
+            }),
+            timestamp(),
+            this.format,
+        ),
+    })
+
+    public info(message: unknown) {
+        this.systemLogger.info(message)
+    }
+
+    public warn(message: unknown) {
+        this.systemLogger.warn(message)
+    }
+
+    public error(message: unknown) {
+        this.systemLogger.error(message)
+    }
+}
