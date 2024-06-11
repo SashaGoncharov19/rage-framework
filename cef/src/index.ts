17ef98cbd291b5d92b1de6c82d949582c6e3ec81
Oleksandr Honcharov <0976053529@ukr.net>
2024-06-11 21:34:35 +0300

    cef implementation

---
+++ cef/src/index.ts
@@ -0,0 +1,15 @@
+import rpc from 'rage-rpc'
+
+class Cef {
+    public register() {}
+
+    public trigger() {}
+
+    public triggerServer() {}
+
+    public triggerClient() {}
+}
+
+export const fw = {
+    event: new Cef(),
+}
