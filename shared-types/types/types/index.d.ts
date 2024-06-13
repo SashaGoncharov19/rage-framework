02c46389b3dd1ecfe32b2142bd83aebf56b5aa7c
Oleksandr Honcharov <0976053529@ukr.net>
2024-06-13 17:04:06 +0300

    cleared index.d.ts

--- shared-types/types/types/index.d.ts
+++ shared-types/types/types/index.d.ts
@@ -1,13 +1,7 @@
 declare module 'rage-fw-shared-types' {
-    export interface RageFW_ICustomServerEvent {
-        test(test: string): void
-    }
+    export interface RageFW_ICustomServerEvent {}
 
-    export interface RageFW_ICustomClientEvent {
-        test(test: string): void
-    }
+    export interface RageFW_ICustomClientEvent {}
 
-    export interface RageFW_ICustomCefEvent {
-        test(test: string): void
-    }
+    export interface RageFW_ICustomCefEvent {}
 }
