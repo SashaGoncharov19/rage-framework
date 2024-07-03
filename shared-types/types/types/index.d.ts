f08291eace3f08260ee5b8ff7e7a12529dd8ba57
Oleksandr Honcharov <0976053529@ukr.net>
2024-07-03 14:43:24 +0300

    deleted types

--- shared-types/types/types/index.d.ts
+++ shared-types/types/types/index.d.ts
@@ -1,13 +1,7 @@
 declare module 'rage-fw-shared-types' {
-    export interface RageFW_ICustomServerEvent {
-        customServerEvent(arg1: string): number
-    }
+    export interface RageFW_ICustomServerEvent {}
 
-    export interface RageFW_ICustomClientEvent {
-        customClientEvent(arg1: string): number
-    }
+    export interface RageFW_ICustomClientEvent {}
 
-    export interface RageFW_ICustomCefEvent {
-        customCefEvent(arg1: string): number
-    }
+    export interface RageFW_ICustomCefEvent {}
 }
