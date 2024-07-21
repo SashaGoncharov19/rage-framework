1467bb65b0c8ff7024fd6fd7f070d3f362392576
Danya H <dev.rilaxik@gmail.com>
2024-07-21 16:27:37 +0100

    fix: missing types

--- client/src/core/player.ts
+++ client/src/core/player.ts
@@ -8,11 +8,11 @@ import {
     RageFW_CefArgs,
     RageFW_CefEvent,
     RageFW_CefReturn,
-    RageFW_ClientEventArguments,
-    RageFW_ClientEventReturn,
+    RageFW_ClientArgs,
+    RageFW_ClientReturn,
     RageFW_ClientServerEvent,
-    RageFW_ClientServerEventArguments,
-    RageFW_ClientServerEventReturn,
+    RageFW_ClientServerArgs,
+    RageFW_ClientServerReturn,
 } from '../types'
 
 export class Player {
@@ -21,18 +21,18 @@ export class Player {
     public trigger<EventName extends keyof RageFW_ICustomClientEvent>(
         eventName: EventName,
         ...args: _ClientEventHasArgs<EventName> extends true
-            ? [RageFW_ClientEventArguments<EventName>]
+            ? [RageFW_ClientArgs<EventName>]
             : []
-    ): Promise<RageFW_ClientEventReturn<EventName>> {
-        return rpc.call<RageFW_ClientEventReturn<EventName>>(eventName, args)
+    ): Promise<RageFW_ClientReturn<EventName>> {
+        return rpc.call<RageFW_ClientReturn<EventName>>(eventName, args)
     }
 
     public triggerServer<EventName extends RageFW_ClientServerEvent>(
         eventName: EventName,
         ...args: _ServerEventHasArgs<EventName> extends true
-            ? [RageFW_ClientServerEventArguments<EventName>]
+            ? [RageFW_ClientServerArgs<EventName>]
             : []
-    ): Promise<RageFW_ClientServerEventReturn<EventName>> {
+    ): Promise<RageFW_ClientServerReturn<EventName>> {
         return rpc.callServer(eventName, args)
     }
 
